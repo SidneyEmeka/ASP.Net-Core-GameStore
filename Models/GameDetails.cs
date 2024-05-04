16979b7d485925b4ec245e41963f5bef0ecc99ef
Sidswipe <sidswipexyz@gmail.com>
2024-05-04 23:12:40 +0100

    Razor

--- Models/GameDetails.cs
+++ Models/GameDetails.cs
@@ -1,12 +1,20 @@
-﻿namespace GameStore.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Frontend.Models;
 
 public class GameDetails
 {
 public int Id { get; set; }
+
+
+[Required]
+[StringLength(50)]
 public required string Name { get; set; } 
 
+[Required(ErrorMessage = "The Genre Field is required. ")]
 public  string?  GenreId { get; set; }
 
+[Range(1, 100)]
 public decimal Price { get; set; }
 
 public DateOnly ReleaseDate { get; set; }
