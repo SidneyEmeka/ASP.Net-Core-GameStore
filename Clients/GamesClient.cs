5d21fa5222bc40c78a858c6d3be88d3419467317
Sidswipe <sidswipexyz@gmail.com>
2024-05-07 11:29:50 +0100

    Readme

--- Clients/GamesClient.cs
+++ Clients/GamesClient.cs
@@ -35,20 +35,64 @@ public class GamesClient
 
     public void AddGame(GameDetails game)
 
-{
-    ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
-    var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
-
-
-    var gameSummary = new GameSummary
     {
-        Id = games.Count + 1,
-        Name = game.Name,
-        Genre = genre.Name,
-        Price = game.Price,
-        ReleaseDate = game.ReleaseDate
-    };
+        Genre genre = GetGenreById(game.GenreId);
 
-    games.Add(gameSummary);
-}
+        var gameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+
+        games.Add(gameSummary);
+    }
+
+  
+    public GameDetails GetGame(int id)
+    {
+        GameSummary? game = GetGameSummaryById(id);
+
+        var genre = genres.Single(genre => string.Equals(
+            genre.Name,
+            game.Genre,
+            StringComparison.OrdinalIgnoreCase));
+
+        return new GameDetails
+        {
+            Id = game.Id,
+            Name = game.Name,
+            GenreId = genre.Id.ToString(),
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
+    public void UpdateGame(GameDetails updatedGame) 
+    {
+        var genre = GetGenreById(updatedGame.GenreId);
+GameSummary existingGame = GetGameSummaryById(updatedGame.Id);
+
+existingGame.Name = updatedGame.Name;
+existingGame.Price = updatedGame.Price;
+existingGame.ReleaseDate = updatedGame.ReleaseDate;
+existingGame.Genre = genre.Name;
+    }
+
+    private GameSummary GetGameSummaryById(int id)
+    {
+        GameSummary? game = games.Find(game => game.Id == id);
+        ArgumentNullException.ThrowIfNull(game);
+        return game;
+    }
+
+    private Genre GetGenreById(string? id)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(id);
+        return genres.Single(genre => genre.Id == int.Parse(id));
+       
+    }
+
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Game Catalogue
+This is a game catalogue software for a video games company where they display their available games with it's corresponding details like Genre, Price, Release Date e.t.c ; Here the Admins can
+-Add a new game to the catalogue
+-Edit the details of an existing game 
